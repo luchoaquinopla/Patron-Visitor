6146a0bebd96229d0047d5f695315b41c9bfa1cb
luchoaquinopla <luchoaquinopla@gmail.com>
2024-04-10 14:21:32 -0300

    "commit final"

--- __test__/visitor.test.js
+++ __test__/visitor.test.js
@@ -14,20 +14,27 @@ test('visitarProducto', () => {
     const producto1 = new producto('Producto de prueba', 100);
     const visitor = new MiVisitor();
     visitor.aceptar(producto1);
-    expect(visitor.total).toBe(100); // Accede a la propiedad total a través de la instancia de MiVisitor
+    expect(visitor.total).toBe(100)
+    expect(visitor.totalNombre).toBe('Producto de prueba'); // Accede a la propiedad total a través de la instancia de MiVisitor
 });
 test('visitarImpuesto', () => {
     const celular = new producto('celular', 1000);
     const impuesto1 = new impuesto(0.15);
     const visitor = new MiVisitor();
-    visitor.aceptarImpuesto(impuesto1); 
-    expect(visitor.total).toBe(0.15); // Verificar que el total se actualiza correctamente según el impuesto
+    impuesto1.producto = celular;
+    visitor.aceptarImpuesto1(impuesto1); 
+    expect(visitor.totalTasa).toBe(0.15);
+    expect(visitor.total).toBe(1150); // Verificar que el total se actualiza correctamente según el impuesto
 });
 
 test('visitarDescuento', () => {
     const celular = new producto('celular', 1000);
     const descuento1 = new descuento(0.10);
     const visitor = new MiVisitor();
+    descuento1.producto = celular;
     visitor.aceptarDescuento(descuento1); // Aceptar el descuento en el visitor
-    expect(visitor.total).toBe(0.10); // Verificar que el total se actualiza correctamente según el descuento
+    expect(visitor.totalDescuento).toBe(900);
+    expect(visitor.totalCantidad).toBe(0.10); // Verificar que el total se actualiza correctamente según el descuento
 });
+
+
--- src/MiVisitor.js
+++ src/MiVisitor.js
@@ -4,24 +4,33 @@ class MiVisitor extends visitor {
     constructor() {
       super(); // Llama al constructor de la clase base
       this.total = 0;
+      this.totalNombre = ''
+      this.totalTasa = 0;
+      this.totalCantidad = 0;
+      this.totalDescuento = 0;
     }
   
     aceptar(visitarProducto) {
       
-        this.total+= visitarProducto.getPrecio(); // Devuelve el precio del producto para poder utilizarlo en otras operaciones
-    }
-   
-    aceptarImpuesto(visitarImpuesto){
-    
-      this.total+= visitarImpuesto.getTasa();
-  }
-  
-
+        this.total+= visitarProducto.getPrecio();// Devuelve el precio del producto para poder utilizarlo en otras operaciones
+        this.totalNombre += visitarProducto.getNombre();
+      }
   
   aceptarDescuento(visitarDescuento){
-    
-    this.total+= visitarDescuento.getCantidad();
+    const producto = visitarDescuento.producto; // Obtiene el producto al que se aplica el descuento
+    this.totalCantidad = visitarDescuento.getCantidad();
+    const descuentoTotal = producto.getPrecio()*visitarDescuento.getCantidad();
+    this.totalDescuento -= descuentoTotal-producto.getPrecio();
   }
-}
+
+  aceptarImpuesto1(visitarImpuesto) {
+    const producto = visitarImpuesto.producto; // Obtiene el producto al que se aplica el impuesto
+   const impuestoTotal = producto.getPrecio()*visitarImpuesto.getTasa();
+   this.total += impuestoTotal+producto.getPrecio();
+    this.totalTasa += visitarImpuesto.getTasa();
+    }
+  }
+
+
   
   module.exports = { MiVisitor, visitor }; // Exporta las clasesS
